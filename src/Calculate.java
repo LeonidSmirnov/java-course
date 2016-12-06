2bc1aa9b4b41a4d69093611578297def44ed8d4d
LeonidSmirnov <doom3.101995@gmail.com>
2016-12-07 01:17:32 +0600

    culculate delete static main

--- src/Calculate.java
+++ src/Calculate.java
@@ -2,12 +2,25 @@
  * Created by Asus on 05.12.2016.
  */
 public class Calculate {
-    public static void main(String[] args) {
-        System.out.println("Calculate");
-        int first = Integer.valueOf(args[0]);
-        int second = Integer.valueOf(args[1]);
-        int sum = first + second;
-        System.out.println("sum = " + sum);
+    private int result;
+
+    public void sum(int ... params){
+        for (Integer param: params
+             ) {
+            this.result += param;
+            
+        }
 
     }
+
+    public int getResult(){
+        return this.result;
+    }
+
+    public void cleanResult(){
+        this.result =0;
+    }
+
+
+
 }
